eca6d3ac76111f3de9cc7201945c8546ff1ff496
Rawal Khirodkar <rawalkhirodkar@gmail.com>
2021-08-13 22:51:58 -0400

    Update run_openpose.sh

--- openpose/scripts/run_openpose.sh
+++ openpose/scripts/run_openpose.sh
@@ -52,30 +52,34 @@ cp -r ./pose_images /home/rawal/Desktop/multipose2body/openpose/output
 ####-----------------------------------------------------------------
 cd /openpose
 
-rm -rf ./pose_jsons
-rm -rf ./pose_images
-rm -rf /home/ANT.AMAZON.COM/khirawal/Desktop/multipose2body/openpose/output
 
-
-# ## for video
-# ./build/examples/openpose/openpose.bin --video examples/media/video.avi\
-# 					--face --hand \
-# 					--write_json ./output_json \
-# 					--display 0 \
-# 					--write_images ./output_images \
-# 					--disable_blending
-
+# # -----------------------------------------------------
 VIDEO_ID=00000
+mkdir -p /home/ANT.AMAZON.COM/khirawal/Desktop/datasets/basketball/openpose/
+mkdir -p /home/ANT.AMAZON.COM/khirawal/Desktop/datasets/basketball/openpose/${VIDEO_ID}
+
 
 ### for images
 ./build/examples/openpose/openpose.bin --image_dir /home/ANT.AMAZON.COM/khirawal/Desktop/datasets/basketball/raw_frames/${VIDEO_ID}\
 					--write_json ./pose_jsons \
 					--display 0 \
-					--write_images ./pose_images \
+					--write_images ./pose_no_blend \
 					--disable_blending
 
-mkdir /home/ANT.AMAZON.COM/khirawal/Desktop/multipose2body/openpose/output/${VIDEO_ID}
+cp -r ./pose_jsons /home/ANT.AMAZON.COM/khirawal/Desktop/datasets/basketball/openpose/${VIDEO_ID}
+cp -r ./pose_no_blend /home/ANT.AMAZON.COM/khirawal/Desktop/datasets/basketball/openpose/${VIDEO_ID}
 
-cp -r ./pose_jsons /home/ANT.AMAZON.COM/khirawal/Desktop/multipose2body/openpose/output/${VIDEO_ID}
-cp -r ./pose_images /home/ANT.AMAZON.COM/khirawal/Desktop/multipose2body/openpose/output/${VIDEO_ID}
+### for images
+./build/examples/openpose/openpose.bin --image_dir /home/ANT.AMAZON.COM/khirawal/Desktop/datasets/basketball/raw_frames/${VIDEO_ID}\
+					--write_json ./pose_jsons \
+					--display 0 \
+					--write_images ./pose_blend \
+
+cp -r ./pose_blend /home/ANT.AMAZON.COM/khirawal/Desktop/datasets/basketball/openpose/${VIDEO_ID}
+
+
+rm -rf ./pose_jsons
+rm -rf ./pose_no_blend
+rm -rf ./pose_blend
+
 
