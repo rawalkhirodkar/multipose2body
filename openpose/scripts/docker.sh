680854976ca4f6e5c858ff52a5d84a1147304cf5
Rawal Khirodkar <rawalkhirodkar@gmail.com>
2021-08-13 22:37:07 -0400

    Update docker.sh

--- openpose/scripts/docker.sh
+++ openpose/scripts/docker.sh
@@ -9,3 +9,16 @@ docker run -it --rm --gpus all --net=host --name=openpose \
 		-v "/home/rawal:/home/rawal:Z" \
 		cwaffles/openpose
 
+
+docker run -it --rm --gpus all --net=host --name=openpose \
+		-p 8888:8888 \
+		--env="DISPLAY" \
+    	--env="QT_X11_NO_MITSHM=1" \
+		--ipc=host \
+		--privileged \
+		-e XAUTHORITY -e NVIDIA_DRIVER_CAPABILITIES=all \
+		-e DISPLAY  \
+		-v "/home/ANT.AMAZON.COM/khirawal:/home/ANT.AMAZON.COM/khirawal:Z" \
+		cwaffles/openpose
+
+cd /home/ANT.AMAZON.COM/khirawal/Desktop/multipose2body/openpose/scripts
